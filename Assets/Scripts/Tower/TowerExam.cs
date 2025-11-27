623e5cfd43d111db6d7568b0d818abc925481ab7
BosungJu <joobs1127@gmail.com>
2025-11-27 15:34:30 +0900

    feat: data class, json data update.

--- Assets/Scripts/Tower/TowerExam.cs
+++ Assets/Scripts/Tower/TowerExam.cs
@@ -7,6 +7,9 @@ public class TowerExam : MonoBehaviour
 {
     public Controller controller;
 
+    /// <summary>
+    /// 플레이어가 빙의할 타워를 선택할 때.
+    /// </summary>
     private void PossessionOnTower() // player func.
     {
         var rayHits = Physics.RaycastAll(controller.transform.position, controller.transform.forward, 100f); // TODO 컨트롤러의 방향으로 변경. transform.forward => controller 방향
@@ -14,14 +17,5 @@ public class TowerExam : MonoBehaviour
         List<Tower> t = rayHits.Where(r => r.collider.TryGetComponent<Tower>(out var tower)).Select(r => r.collider.GetComponent<Tower>()).ToList();
 
         t[0].Possession();
-    }
-
-    private void ShotMonster() // tower func.
-    {
-        var rayHits = Physics.RaycastAll(controller.transform.position, controller.transform.forward, 100f); // TODO 컨트롤러의 방향으로 변경. transform.forward => controller 방향
-
-        List<MonsterObject> m = rayHits.Where(r => r.collider.TryGetComponent<MonsterObject>(out var monster)).Select(r => r.collider.GetComponent<MonsterObject>()).ToList();
-
-        // target = m[0];
     }
 }
